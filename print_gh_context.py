6caa4bd9c6c7d8eb57423bc4536cf74e1ce2f673
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2025-02-27 18:36:01 +0100

    Fix names of environment vars?

--- print_gh_context.py
+++ print_gh_context.py
@@ -3,7 +3,7 @@ import os
 print("Hello World!")
 
 print(os.environ)
-print(f"{os.environ['github.head_ref']}")
-print(f"{os.environ['github.sha']}")
+print(f"{os.environ['GITHUB_HEAD_REF']}")
+print(f"{os.environ['GITHUB_SHA']}")
 
 print("Byebye!")
