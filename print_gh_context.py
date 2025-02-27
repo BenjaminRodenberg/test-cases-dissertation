706b00c8d71d42b80e04f5843a804ae43073efce
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2025-02-27 18:40:50 +0100

    Set environment in action.

--- print_gh_context.py
+++ print_gh_context.py
@@ -3,7 +3,7 @@ import os
 print("Hello World!")
 
 print(os.environ)
-print(f"{os.environ['GITHUB_HEAD_REF']}")
-print(f"{os.environ['GITHUB_SHA']}")
+print(f"{os.environ['MY_HEAD_REF']}")
+print(f"{os.environ['MY_SHA']}")
 
 print("Byebye!")
